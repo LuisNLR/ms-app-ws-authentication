63095717ff3f60e98e10f50415d80a59270a8529
noe Lizana <luisnoe6@hotmail.com>
2025-03-17 15:18:03 -0500

    update to .gitignore and remove test main

--- src/main/java/pe/com/softlite/authentication/service/imp/Practice.java
+++ src/main/java/pe/com/softlite/authentication/service/imp/Practice.java
@@ -4,21 +4,21 @@ import pe.com.softlite.authentication.utils.PasswordUtils;
 
 public class Practice {
 	
-	public static void main(String args[]) {
-//		String hashedPassword = PasswordUtils.hashPassword("mi_contraseña");
-		String hashedPassword = PasswordUtils.hashPassword("personal123");
-		System.out.println("mi_contraseña Pass Hash: " + hashedPassword);
-		
-		//String hashedPasswordIngresada = PasswordUtils.hashPassword("personal123");
-		//System.out.println("mi_contraseña_ingresada Pass Hash: " + hashedPasswordIngresada);
-		
-		boolean isPasswordValid = PasswordUtils.matchPassword("personal123", hashedPassword);
-		if (isPasswordValid) {
-		    System.out.println("¡Contraseña válida!");
-		} else {
-		    System.out.println("Contraseña incorrecta");
-		}
-	}
+//	public static void main(String args[]) {
+////		String hashedPassword = PasswordUtils.hashPassword("mi_contraseña");
+//		String hashedPassword = PasswordUtils.hashPassword("personal123");
+//		System.out.println("mi_contraseña Pass Hash: " + hashedPassword);
+//		
+//		//String hashedPasswordIngresada = PasswordUtils.hashPassword("personal123");
+//		//System.out.println("mi_contraseña_ingresada Pass Hash: " + hashedPasswordIngresada);
+//		
+//		boolean isPasswordValid = PasswordUtils.matchPassword("personal123", hashedPassword);
+//		if (isPasswordValid) {
+//		    System.out.println("¡Contraseña válida!");
+//		} else {
+//		    System.out.println("Contraseña incorrecta");
+//		}
+//	}
 	
 	
 
